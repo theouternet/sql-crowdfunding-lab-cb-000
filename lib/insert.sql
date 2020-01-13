dbf1007144e48a33bce1d1259ea740ec444f60a2
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-13 23:20:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,7 +9,3 @@ INSERT INTO projects VALUES(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-3
 INSERT INTO projects VALUES(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30');
 INSERT INTO projects VALUES(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
-(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
-(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
-(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
-(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
